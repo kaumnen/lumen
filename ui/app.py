613b5e9355a89a0599d2c8693e8fddabe8531b35
kaumnen <dev@kaumnen.com>
2025-04-20 10:31:12 +0000

    initial ui structure

--- ui/app.py
+++ ui/app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+st.set_page_config(page_title="lumen", page_icon="🟡")
 
+pdf = st.Page("rag/vectorize_pdf.py", title="Add PDF", icon="📄")
 
-st.title("Lumen")
+settings = st.Page("general/settings.py", title="Settings", icon="⚙️")
+
+home = st.Page("general/home.py", title="Home", icon="🏠")
+
+pg = st.navigation({"Home": [home], "Vectors": [pdf], "Settings": [settings]})
+
+pg.run()
