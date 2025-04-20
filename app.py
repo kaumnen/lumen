2fc098b5b93f3511ee2f7937d2213052fa88a1e5
kaumnen <dev@kaumnen.com>
2025-04-20 19:19:28 +0000

    update file pointer

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="lumen", page_icon="🟡", layout="wide")
 
 home = st.Page("ui/general/home.py", title="Home", icon="🏠")
 search_vectors = st.Page("ui/rag/search.py", title="Search Vectors", icon="🔍")
-pdf = st.Page("ui/rag/vectorize_pdf.py", title="Add AWS Docs PDF", icon="👉")
+pdf = st.Page("ui/rag/add_pdf.py", title="Add AWS Docs PDF", icon="👉")
 settings = st.Page("ui/general/settings.py", title="Settings", icon="⚙️")
 
 pg = st.navigation(
