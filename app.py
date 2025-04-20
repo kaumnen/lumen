863b8a5d41887194914231ee429ca135ac800493
kaumnen <dev@kaumnen.com>
2025-04-20 12:23:17 +0000

    restructure initializer

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="lumen", page_icon="🟡", layout="wide")
+
+home = st.Page("ui/general/home.py", title="Home", icon="🏠")
+search_vectors = st.Page("ui/rag/search.py", title="Search Vectors", icon="🔍")
+pdf = st.Page("ui/rag/vectorize_pdf.py", title="Add AWS Docs PDF", icon="👉")
+settings = st.Page("ui/general/settings.py", title="Settings", icon="⚙️")
+
+pg = st.navigation(
+    {"Home": [home], "Vectors": [pdf, search_vectors], "Settings": [settings]}
+)
+
+pg.run()
