d98504be039703547dbe56d848593e741f14c71c
kaumnen <dev@kaumnen.com>
2025-04-20 14:48:47 +0000

    add toast instead of banner

--- ui/rag/vectorize_pdf.py
+++ ui/rag/vectorize_pdf.py
@@ -16,7 +16,7 @@ if submitted:
     if url_input:
         parsed_url = urlparse(url_input)
         if parsed_url.scheme == "https" and "docs.aws.amazon.com" in parsed_url.netloc:
-            st.success(f"URL Valid: {url_input}")
+            st.toast(f"URL Valid: {url_input}")
             with st.spinner("Processing..."):
                 ingest_chunks_from_pdf(url_input)
             st.success("PDF processed and vectorized successfully!")
