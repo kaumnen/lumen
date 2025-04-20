9ee8fe15bda95748c390f15eb9b3e4dd2c3a6dcc
kaumnen <dev@kaumnen.com>
2025-04-20 12:22:48 +0000

    update vector parser page

--- ui/rag/vectorize_pdf.py
+++ ui/rag/vectorize_pdf.py
@@ -1,3 +1,29 @@
 import streamlit as st
+from urllib.parse import urlparse
+from src.vector_store.qdrant_manager import ingest_chunks_from_pdf
 
-st.write("# Vectorize PDF! 👋")
+st.title("Add AWS Docs PDF")
+st.markdown(
+    "This page allows you to add a PDF from AWS documentation. The PDF will be:\n- parsed\n- vectorized and\n- stored in the vector database"
+)
+
+with st.form(key="url_form"):
+    url_input = st.text_input("Enter URL:")
+
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+    if url_input:
+        parsed_url = urlparse(url_input)
+        if parsed_url.scheme == "https" and "docs.aws.amazon.com" in parsed_url.netloc:
+            st.success(f"URL Valid: {url_input}")
+            with st.spinner("Processing..."):
+                ingest_chunks_from_pdf(url_input)
+            st.success("PDF processed and vectorized successfully!")
+        else:
+            st.error(
+                "**Invalid URL**\n\nThe URL must:\n- Use `HTTPS`\n- Be on the `docs.aws.amazon.com` domain",
+                icon="🚫",
+            )
+    else:
+        st.warning("Please enter a URL.")
