d4e033b83d019c2c2adc03d5d9b2d4a7a93ab0ec
kaumnen <dev@kaumnen.com>
2025-04-21 19:54:54 +0000

    remove not needed pages

--- app.py
+++ app.py
@@ -2,19 +2,14 @@ import streamlit as st
 
 st.set_page_config(page_title="lumen", page_icon="🟡", layout="wide")
 
-home = st.Page("ui/general/home.py", title="Home", icon="🏠")
-
 chat = st.Page("ui/rag/chat.py", title="Chat", icon="💬")
 search_vectors = st.Page("ui/rag/search.py", title="Search Vectors", icon="🔍")
 pdf = st.Page("ui/rag/add_pdf.py", title="Add AWS Docs PDF", icon="👉")
-settings = st.Page("ui/general/settings.py", title="Settings", icon="⚙️")
 
 pg = st.navigation(
     {
-        "Home": [home],
-        "Vectors": [pdf, search_vectors],
         "Chat": [chat],
-        "Settings": [settings],
+        "Vectors": [pdf, search_vectors],
     }
 )
 
