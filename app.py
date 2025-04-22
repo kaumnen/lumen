127b338636f2fe4459469b06f7cda1b1a72eb6d1
kaumnen <dev@kaumnen.com>
2025-04-22 06:10:07 +0000

    add somewhat of a logo..

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 st.set_page_config(page_title="lumen", page_icon="🟡", layout="wide")
+st.logo(
+    '<svg viewBox="0 0 130 36" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="iconify iconify--twemoji"><g stroke-width="0"/><g stroke-linecap="round" stroke-linejoin="round"/><circle fill="#FDCB58" cx="18" cy="18" r="18"/><text x="50" y="20" font-size="30" font-family="Arial, sans-serif" dominant-baseline="middle">lumen</text></svg>'
+)
 
 chat = st.Page("ui/rag/chat.py", title="Chat", icon="💬")
 search_vectors = st.Page("ui/rag/search.py", title="Search Vectors", icon="🔍")
