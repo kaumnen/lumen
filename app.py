fe66d592d8cdf1f75bf1a2ff79dc5145e6a088a9
kaumnen <dev@kaumnen.com>
2025-04-25 14:44:01 +0000

    update namings

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ st.logo(
     '<svg viewBox="0 0 130 36" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="iconify iconify--twemoji"><g stroke-width="0"/><g stroke-linecap="round" stroke-linejoin="round"/><circle fill="#FDCB58" cx="18" cy="18" r="18"/><text x="50" y="20" font-size="30" font-family="Arial, sans-serif" dominant-baseline="middle">lumen</text></svg>'
 )
 
-chat = st.Page("ui/rag/chat.py", title="Chat", icon="💬")
+chat = st.Page("ui/rag/chat.py", title="Agent Chat", icon="🤖")
+mcp_chat = st.Page(
+    "ui/mcp/chat.py", url_path="mcp_chat", title="MCP Chat", icon="🖇️"
+)
 search_vectors = st.Page("ui/rag/search.py", title="Search Vectors", icon="🔍")
-pdf = st.Page("ui/rag/add_pdf.py", title="Add AWS Docs PDF", icon="👉")
+pdf = st.Page("ui/rag/add_pdf.py", title="Add AWS Docs", icon="➕")
 
 pg = st.navigation(
     {
-        "General": [chat],
+        "General": [chat, mcp_chat],
         "Vectors": [pdf, search_vectors],
     }
 )
