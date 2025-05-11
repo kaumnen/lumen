dafdf2b0e8a304956409c06c8f97550a20ca7427
kaumnen <dev@kaumnen.com>
2025-05-11 15:51:01 +0000

    improve logs

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from loguru import logger
+import sys
+
+logger.remove()
+logger.add(
+    sys.stderr,
+    format="{time} | <level>{level}</level> | {file}:{line} | {module}:{function} | <level>{message}</level>",
+    colorize=True,
+)
 
 st.set_page_config(page_title="lumen", page_icon="🟡", layout="wide")
 st.logo(
@@ -6,9 +15,7 @@ st.logo(
 )
 
 chat = st.Page("ui/rag/chat.py", title="Agent Chat", icon="🤖")
-mcp_chat = st.Page(
-    "ui/mcp/chat.py", url_path="mcp_chat", title="MCP Chat", icon="🖇️"
-)
+mcp_chat = st.Page("ui/mcp/chat.py", url_path="mcp_chat", title="MCP Chat", icon="🖇️")
 search_vectors = st.Page("ui/rag/search.py", title="Search Vectors", icon="🔍")
 pdf = st.Page("ui/rag/add_pdf.py", title="Add AWS Docs", icon="➕")
 
